a5f6206bf4a0317b9ad74184ea6f104838fd7fa9
Mary <mary.taranuha@yandex.ru>
2021-11-15 15:29:42 +0800

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone},order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
 
@@ -47,7 +47,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    JSON.stringify({data: data});
 
-    return jsonData;
+    return json.data;
 }
